60eb34450f7a3e197db7499ffc026ddac8989883
yangpanteng <pantengyang@cybstar.com>
2021-11-17 11:20:50 +0800

    feat(packages): 适配tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
