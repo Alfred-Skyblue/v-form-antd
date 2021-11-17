bb9131307e3800790c407526470f824459f5e2f2
yangpanteng <pantengyang@cybstar.com>
2021-11-17 11:20:29 +0800

    feat(packages): 适配tailwindCSS

--- examples/main.ts
+++ examples/main.ts
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCompositionAPI from '@vue/composition-api'
-
+import formDesign from '../packages/index'
 Vue.use(VueCompositionAPI)
+Vue.use(formDesign)
 Vue.config.productionTip = false
 
 new Vue({
