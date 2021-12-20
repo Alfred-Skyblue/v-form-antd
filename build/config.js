c91d08df7ecd39861137c9c36a9512c196e61f2a
yangpanteng <pantengyang@cybstar.com>
2021-12-20 18:10:09 +0800

    revert: 修改runtime.css文件为index.css

--- build/config.js
+++ build/config.js
@@ -5,7 +5,7 @@ const url = path.join(__dirname, '../lib/')
 function cssRename() {
   const oldFile = `${url + pkg.name}-runtime.css`
   const newFile = `${url}index.css`
-  fs.rename(oldFile, newFile, (err: Error) => {
+  fs.rename(oldFile, newFile, err => {
     if (err) throw err
   })
 }
