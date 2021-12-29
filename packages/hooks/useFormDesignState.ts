709eb212ad69bb04a8413100e00cdc57d55f9e63
yangpanteng <893042011@qq.com>
2021-12-29 22:14:14 +0800

    docs(packages/hooks/useFormDesignState.ts): 注释

--- packages/hooks/useFormDesignState.ts
+++ packages/hooks/useFormDesignState.ts
@@ -2,6 +2,9 @@ import { inject, Ref } from '@vue/composition-api'
 import { IFormConfig } from '@pack/typings/v-form-component'
 import { IFormDesignMethods } from '@pack/VFormDesign/index.vue'
 
+/**
+ * 获取formDesign状态
+ */
 export function useFormDesignState() {
   const formConfig = inject('formConfig') as Ref<IFormConfig>
   const formDesignMethods = inject('formDesignMethods') as IFormDesignMethods
