8fc370478d73c04776503f670e1025523efe842a
yangpanteng <pantengyang@cybstar.com>
2021-12-30 18:26:17 +0800

    refactor(packages/mini.ts): 不到导出formItem组件

--- packages/mini.ts
+++ packages/mini.ts
@@ -2,26 +2,22 @@ import '../styles/index.less'
 import './assets/iconfont'
 
 import VFormDesign from './VFormDesign'
-import VIconPicker from './VIconPicker'
-import VFormItem from './VFormItem'
 import VFormCreate from './VFormCreate'
 import pkg from '/package.json'
 
 import { VueConstructor } from 'vue'
 import { setFormDesignComponents } from '@pack/core/formItemConfig'
-const components = [VFormDesign, VFormItem, VFormCreate]
+const components = [VFormDesign, VFormCreate]
 const install = (Vue: VueConstructor) => {
   components.forEach(component => {
     Vue.component(component.name, component)
   })
 }
-export { VFormDesign, VFormItem, VIconPicker, setFormDesignComponents, VFormCreate }
+export { VFormDesign, setFormDesignComponents, VFormCreate }
 export default {
   install,
   version: pkg.version,
   VFormDesign,
-  VFormItem,
-  VIconPicker,
   setFormDesignComponents,
   VFormCreate
 }
