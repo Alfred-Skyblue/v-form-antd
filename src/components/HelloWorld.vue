bcec35d25c01914f68b6b75ea49209a45576db85
丶远方 <yangpanteng@gmail.com>
2022-12-03 12:10:18 +0800

    test：测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<!--
+ * @author: ypt
+ * @date: 2022/5/26
+ * @description: $END$
+-->
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  message: {
+    type: String,
+    default: 'Hello World!'
+  }
+})
+</script>
